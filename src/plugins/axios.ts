25bd8e203fa047bf0d19117bbf279c09c7c160e4
AdrianchoF <felipebaquero28@gmail.com>
2026-05-13 01:51:52 -0500

    configuracion para desplegar app

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Instancia base de Axios para tu API NestJS
 const api = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true, // habilítalo si usas cookies
 });
 
